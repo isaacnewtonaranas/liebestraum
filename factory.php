8c86df396f3cd3dc5d0250fbea434c73115ad715
airamaeay <airamaearanas@gmail.com>
2021-02-10 18:07:45 +0800

    config and factory

--- factory.php
+++ factory.php
@@ -2,9 +2,12 @@
     require "config.php";
 
     for($n = 0;$n < 10;$n++){
-       
-       
-        $random_name = strtolower(randomName())."_".strtolower(randomName());
+        $random_username = strtolower(randomName())."_".strtolower(randomName());
+        $first_name = randomName();
+        $last_name = randomName();
+        $birth_day = randomMonthd()." ".randomDays().",".randomYears();
+        $random_username = strtolower(randomName())."_".strtolower(randomName());
+        $hobbies = randomHobbies();
        // $tindex = rand(3,5);
        // $random_password = substr($random_name,0,$tindex).rand(100,999);
        // mysqli_query($con,"INSERT INTO `users` (`username`, `password`) VALUES ('$random_name', '$random_password')");
