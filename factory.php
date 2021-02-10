7aed262585c296b8995370abeafa302938a20c32
A Star Vlog <admin@astarvlog.com>
2021-02-10 18:42:00 +0800

    query created

--- factory.php
+++ factory.php
@@ -10,5 +10,6 @@
         $hobbies = randomHobbies();
        // $tindex = rand(3,5);
        // $random_password = substr($random_name,0,$tindex).rand(100,999);
-       // mysqli_query($con,"INSERT INTO `users` (`username`, `password`) VALUES ('$random_name', '$random_password')");
+       mysqli_query($con,"INSERT INTO `users`(`username`, `fname`, `lname`, `birthday`, `hobbies`)
+       VALUES ('$random_username','$first_name','$last_name','$birth_day','$hobbies')");
     }
