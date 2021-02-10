3dd0a560a35ddc30e2b682d4f514854ea5daa06a
elijaharanas12 <zeprotechnogaming12@gmail.com>
2021-02-11 01:14:34 +0800

    script

--- my-page.php
+++ my-page.php
@@ -8,9 +8,12 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js"></script>
+
 </head>
 <body class="p-5">
-    <h3>Personal Card Details</h3>
+    <!--<h3>Personal Card Details</h3>
     <p>
     Do not share your personal information such as your user name, passwords, and e-mail address with anybody. Do not share any of your credit card details such as the 3-digit security code found at the back of the card and/or the credit card expiry date. Change your PIN regularly, and do not share it with anyone.
     </p>
@@ -19,7 +22,7 @@
     <br>
     Your card expiration date is 07/27
     <br>
-    Your card CVV is 123
+    Your card CVV is 123-->
     <h5 style="margin-top:30px;">Users</h5>
     <div class="js_users">
     </div>
