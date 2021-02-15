085b34d438a54a774b4a40e10bc726fead11ed6c
elijaharanas12 <zeprotechnogaming12@gmail.com>
2021-02-15 18:59:13 +0800

    my-page

--- my-page.php
+++ my-page.php
@@ -8,7 +8,8 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js"></script>
 </head>
 <body class="p-5">
     <!--<h3>Personal Card Details</h3>
@@ -41,10 +42,11 @@
         $.get("http://localhost/liebestraum/users/",function(data){
             console.log(data)
             for(i in data){
+                
                 $(".db_users").append(
                     `<tr>
-                        <td>` + data[i].first_name + `</td>
-                        <td>` + data[i].last_name + `</td>
+                        <td>` + data[i].fname + `</td>
+                        <td>` + data[i].lname + `</td>
                         <td>` + data[i].birthday + `</td>
                         <td>` + data[i].username + `</td>
                         <td>` + data[i].hobbies + `</td>
@@ -52,6 +54,9 @@
                 )
             }
         })
+        $(document).ready( function () {
+            $('#table_id').DataTable();
+        } );
     </script>
 </body>
 </html>
