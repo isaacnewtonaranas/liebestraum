b09a3a187faa46593111e6338a736db5d1368280
airamaeay <airamaearanas@gmail.com>
2021-02-16 19:30:10 +0800

    slq delete-musiciansl

--- factory.php
+++ factory.php
@@ -2,7 +2,7 @@
     require "config.php";
     require "functions.php";
 
-    mysqli_query($con,"DELETE FROM `users`,`musicians`");
+    mysqli_query($con,"DELETE FROM `users`");
    
 
     for($n = 0;$n < 10;$n++){
@@ -17,6 +17,8 @@
        mysqli_query($con,"INSERT INTO `users`(`username`, `fname`, `lname`, `birthday`, `hobbies`)
        VALUES ('$random_username','$first_name','$last_name','$birth_day','$hobbies')");
     }
+
+    mysqli_query($con,"DELETE FROM `musicians`");
 
     for($n = 0;$n < 10;$n++){
         
