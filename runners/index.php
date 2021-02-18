cca6540fe2acab621f8f7832e8ff190e0d9021ca
airamaeay <airamaearanas@gmail.com>
2021-02-18 18:11:13 +0800

    index.php

--- runners/index.php
+++ runners/index.php
@@ -1,7 +1,7 @@
 <?php
     require "../config.php";
 
-    $result = mysqli_query($con,"SELECT * FROM users");
+    $result = mysqli_query($con,"SELECT * FROM runners");
     $all = mysqli_fetch_all($result,MYSQLI_ASSOC);
     // var_dump($all);
     header('Content-Type: application/json');
